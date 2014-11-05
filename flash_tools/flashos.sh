93a6bf75e9db9105c404845fb86db01fedfe0958
Rex Shen <mupingshen@gmail.com>
2014-11-05 22:50:52 +0800

    fix a typo in flashos.sh

--- flash_tools/flashos.sh
+++ flash_tools/flashos.sh
@@ -2,7 +2,7 @@
  SYSTYPE=$(uname -s)
  if [ $SYSTYPE = Linux ]; then
  #sudo ./nvflash --bct bct.cfg --setbct --odmdata 0x80098000 --configfile flash.cfg --create --bl bootloader.bin --go
- sudo ./nvflash_linx --bct bct.cfg --setbct --configfile flash.cfg --create --bl bootloader.bin --odmdata 0x6009C000 --go
+ sudo ./nvflash_linux --bct bct.cfg --setbct --configfile flash.cfg --create --bl bootloader.bin --odmdata 0x6009C000 --go
  elif [ $SYSTYPE = Darwin ]; then
  ./nvflash_macosx --bct bct.cfg --setbct --odmdata 0x80098000 --configfile flash.cfg --create --bl bootloader.bin --go
  fi
